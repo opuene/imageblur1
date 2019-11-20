5725cc729d0a09e4c629839069396bda362ee2fb
opuene <opusfibz@gmail.com>
2019-11-20 14:02:38 +0000

    image blur challenge

---
+++ imageblur1.rb
@@ -0,0 +1,22 @@
+class Image
+  attr_accessor :picture
+
+  def initialize(picture)
+    @picture = picture
+  end
+
+  def output_image
+    @picture.each do |data|
+      puts data.join
+    end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
